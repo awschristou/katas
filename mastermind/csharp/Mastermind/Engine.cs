43f503ed959649fd1097b62de466734cea14ceca
Christopher Christou <chrstou@amazon.com>
2022-03-10 14:57:46 -0800

    Some tests
    Additional co-authors: Shruti, Ralph
    
    Co-authored-by: Rishi Barad <rbbarad@users.noreply.github.com>
    Co-authored-by: JadenSimon <JadenSimon@users.noreply.github.com>

--- mastermind/csharp/Mastermind/Engine.cs
+++ mastermind/csharp/Mastermind/Engine.cs
@@ -1,12 +1,40 @@
 ﻿namespace Mastermind;
 
-public record Results {
+// Typist / Navigator / Next
+// CC
+// JS
+// RB
+// RF
+// SS
 
-}
+
+public record Results (int Correct, int Misplaced);
+// public record Results {
+//     int Correct;
+//     int Misplaced; // but correct
+// }
 
 public class Engine
 {
-    public Results Evaluate() {
-        return new Results();
+    public int Misplaced(string[] secret, string[] guess) {
+        //return Enumerable.Range(0, secret.Count()).ToList().Count(n => secret[n] == guess[n]);
+        int misplaced = 0;
+        Dictionary<string, int> colorCounts = secret.ToList().GroupBy(c => c).ToDictionary(group => (group.Key), group => group.Count());
+
+        for (int i = 0; i < secret.Count(); i++) {
+            if (secret[i] != guess[i] && secret.Contains(guess[i]) && colorCounts[guess[i]]!=0) {
+                misplaced++;
+                colorCounts[guess[i]] = colorCounts[guess[i]]-1;
+            }
+        }
+        return misplaced;
+    }
+
+    public int WellPlaced(string[] secret, string[] guess) {
+        return Enumerable.Range(0, secret.Count()).ToList().Count(n => secret[n] == guess[n]);
+    }
+
+    public Results Evaluate(string[] secret, string[] guess) {
+        return new Results(WellPlaced(secret, guess), Misplaced(secret, guess));
     }
 }
--- mastermind/csharp/Tests/EngineTests.cs
+++ mastermind/csharp/Tests/EngineTests.cs
@@ -5,11 +5,73 @@ namespace Tests;
 
 public class EngineTests
 {
+
+    // Therefore, your function should return, 
+    // for a secret and a guessing combination:
+
+    // the number of well placed colors
+    // the number of correct but misplaced colors
+
     private readonly Engine _sut = new Engine();
 
+    // [Fact]
+    // public void Test1()
+    // {
+    //     // Assert.NotNull(_sut.Evaluate());
+    // }
+
     [Fact]
-    public void Test1()
+    public void NoMatch()
     {
-        Assert.NotNull(_sut.Evaluate());
+        var result = _sut.Evaluate(
+            new [] {"blue"}, 
+            new [] {"red"}
+        );
+
+        Assert.Equal(new Results(0, 0), result);
     }
+
+    
+    [Fact]
+    public void WellPlaced()
+    {
+        var result = _sut.Evaluate(
+            new [] {"blue"}, 
+            new [] {"blue"}
+        );
+
+        Assert.Equal(new Results(1, 0), result);
+    }
+
+    [Fact]
+    public void Misplaced()
+    {
+        var result = _sut.Evaluate(
+            new [] {"red", "yellow"}, 
+            new [] {"blue", "red"}
+        );
+
+        Assert.Equal(new Results(0, 1), result);
+    }
+
+    [Fact]
+    public void MisplacedWithRepeatColors()
+    {
+        var result = _sut.Evaluate(
+            new [] {"red", "red", "yellow", "yellow", "yellow"}, 
+            new [] {"green", "green", "red", "red", "red"}
+        );
+
+        Assert.Equal(new Results(0, 2), result);
+    }
+
+    // but what happens when there is:
+    // one match and one mismatch
+    // r r y vvvv r g r (1, 1)
 }
+
+
+// evaluate([blue], [red]) should return [0, 0]  
+// evaluate([blue], [blue]) should return [1, 0]  
+// evaluate([red, yellow], [blue, red]) should return [0, 1]  
+// r r g b vs x x r x -> 1
